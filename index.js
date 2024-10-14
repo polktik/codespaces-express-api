680607dd7797e9a5631e843e952e6e8b51d279f4
polktik <157307720+polktik@users.noreply.github.com>
2024-10-14 08:10:23 +0000

    add GET endpoint

--- index.js
+++ index.js
@@ -1,45 +1,93 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 5000;
+var mysql = require('mysql');
+const port =5000;
 
+var con = mysql.createConnection({
+  host: "korawit.ddns.net",
+  user: "webapp",
+  password: "secret2024",
+  port: 3307,
+  database: "shop"
+});
+
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accpet, Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE,"
+    "GET, POST, PUT, DELETE"
   );
   next();
 });
-
 app.use(express.json());
 
-const products=[
-  {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-  {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-  {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-  {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-  {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-  {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
+
+
+con.connect(function (err) {
+  if (err) throw err;
+});
+
+app.get('/api/products', function(req,res){
+  con.query("SELECT * FROM products", function(err, result, fields) {
+    if (err) throw res.status(400).send('Not found any products');
+    console.log(result);
+    res.send(result);
+  });
+});
+  
+app.get('/api/products/:id', function(req,res) {
+  const id = req.params.id;
+  con.query("SELECT * FROM products WHERE id="+id, function(err, result, fields) {
+    if (err) throw err;
+    let products = result;
+    if(products.length>0) {
+      res.send(products);
+    }
+    else {
+      res.status(400).send('Not found product for'+id);
+    }
+    console.log(result);
+  });
+});
+
+// app.delete('/api/products/:id', function(req, res) {
+//   const id = req.params.id;
+//   con.query("DELETE FROM products WHERE id="+id, function(err, result, fields) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM products", function(err, result, fields) {
+//       res.send(result);
+//       console.log(result);
+//     });
+//   });
+// });
+
+// const products = [
+//   { id: 0, name: "Notebook Acer Swift", price: 45900, img: "https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg" },
+//   { id: 1, name: "Notebook Asus Vivo", price: 19900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg" },
+//   { id: 2, name: "Notebook Lenovo Ideapad", price: 32900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg" },
+//   { id: 3, name: "Notebook MSI Prestige", price: 54900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg" },
+//   { id: 4, name: "Notebook DELL XPS", price: 99900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg" },
+//   { id: 5, name: "Notebook HP Envy", price: 46900, img: "https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg" }];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/products',(req, res) => {
-  if(products.length > 0) {
-    res.send(products);
-  }
-  else {
-    res.status(400).send('Not found product for'+id);
-  }
-})
+// app.get('/api/products', (req, res) => {
+//   if (products.length > 0) {
+//     res.send(products);
+//   }
+//   else {
+//     res.status(400).send('Not found product for' + id);
+//   }
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
